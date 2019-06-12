9f0ee72916cb91d6dd2a095f984f0d4432c502b5
lgm527 <laurellmccaffrey@gmail.com>
2019-06-12 14:47:20 -0400

    green grocer is now ready, like I, Dr. Steve Bruhle

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,64 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+  count = :count
+  cart.each do |i|
+    i.each do |j, h|
+        if !new_cart.has_key?(j)
+          new_cart[j] = h
+          new_cart[j][:count] = 1
+        else
+          new_cart[j][:count] += 1
+        end
+    end
+  end
+  new_cart
 end
 
+#This is k AVOCADO
+#This is v {:price=>3.0, :clearance=>true, :count=>2}
+
 def apply_coupons(cart, coupons)
-  # code here
+  coup_cart = {}
+  cart.each do |k, v|
+    coupons.each do |i|
+      if k == i[:item] && v[:count] >= i[:num]
+        v[:count] = v[:count] - i[:num]
+        if coup_cart["#{k} W/COUPON"]
+          coup_cart["#{k} W/COUPON"][:count] += 1
+        else
+          coup_cart["#{k} W/COUPON"] = {price: i[:cost], clearance: v[:clearance], count: 1}
+        end
+      end
+    end
+    coup_cart[k] = v
+  end
+  coup_cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |k, v|
+    if v[:clearance] == true
+      v[:price] = (v[:price] * 0.8).round(1)
+    end
+  end
+  cart
 end
 
+# i = "BEETS"
+# info = {:price=>2.5, :clearance=>false, :count=>1}
+
 def checkout(cart, coupons)
-  # code here
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  cart_total = 0
+
+  cart.each do |i, info|
+    cart_total += ((info[:price] * info[:count]).to_f).round(1)
+  end
+  if cart_total > 100
+    return (cart_total * 0.9)
+  else
+    return cart_total
+  end
 end
