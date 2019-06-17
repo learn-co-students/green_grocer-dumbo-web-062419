78df3d1d3eb72bc1dd3e1dbb260f37359f2e3221
Olga Rosas <olga.rosas@gmail.com>
2019-06-17 16:39:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,17 +1,16 @@
 require 'pry'
 
-def consolidate_cart(cart) 
+def consolidate_cart(cart)
   consolidated_hash = {}
   cart.each do |item|
     item.each do |item_name, item_hash|
         if consolidated_hash.key?(item_name) == false
           consolidated_hash[item_name] = item_hash
           consolidated_hash[item_name][:count] = 1
-        else 
+        else
           consolidated_hash[item_name][:count] += 1
         end
     end
   end
   consolidated_hash
 end
-
