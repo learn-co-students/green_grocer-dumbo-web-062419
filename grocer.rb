116f81bf85140098d2c549350543212b8e4486c1
Olga Rosas <olga.rosas@gmail.com>
2019-06-17 16:47:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,8 +14,41 @@ def consolidate_cart(cart)
   hash
 end
 
+
 def apply_coupons(cart, coupons)
-# code here
+  hash = cart
+  coupons.each do |coupon_hash|
+    # add coupon to cart
+    item = coupon_hash[:item]
+
+    if !hash[item].nil? && hash[item][:count] >= coupon_hash[:num]
+      temp = {"#{item} W/COUPON" => {
+        :price => coupon_hash[:cost],
+        :clearance => hash[item][:clearance],
+        :count => 1
+        }
+      }
+      
+      if hash["#{item} W/COUPON"].nil?
+        hash.merge!(temp)
+      else
+        hash["#{item} W/COUPON"][:count] += 1
+        #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
+      end
+      
+      hash[item][:count] -= coupon_hash[:num]
+    end
+  end
+  hash
+end
+
+def apply_clearance(cart)
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
