2fcb2af8d6a1c5229c5a29a0a8347ad7a8c089b3
Olga Rosas <olga.rosas@gmail.com>
2019-06-17 16:18:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,8 +3,8 @@ def consolidate_cart(cart)
   count = 0
   cart.each do |element|
     element.each do |fruit, hash|
-      organized_cart[fruit] || = hash
-      organized_cart[fruit][:count] || = 0
+      organized_cart[fruit] ||= hash
+      organized_cart[fruit][:count] ||= 0
       organized_cart[fruit][:count] += 1
     end
   end
@@ -12,13 +12,23 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon_hash|
+    fruit_name = coupon_hash[:item]
+    new_coupon_hash = {
+      :price => coupon_hash[:cost],
+      :clearance => "true",
+      :count => coupon_hash[:num]
+    }
+    
+     if cart.key?(fruit_name)
+      new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
+      if cart[fruit_name][:count]>= new_coupon_hash[:count]
+        new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
+        cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
+      end
+      cart[fruit_name + " W/COUPON"] = new_coupon_hash 
+    end
+    end
+  return cart
 end
 
-def apply_clearance(cart)
-  # code here
-end
-
-def checkout(cart, coupons)
-  # code here
-end
