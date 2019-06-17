d831f8ad12bb33b5202d899ed7d866eb830eae82
Olga Rosas <olga.rosas@gmail.com>
2019-06-17 16:25:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,22 @@
 require 'pry'
 
+
 def consolidate_cart(cart)
-  #binding.pry
+  organized_cart = {}
+  cart.each do |element|
+    element.each do |fruit, hash|
+      organized_cart[fruit] ||= hash
+      organized_cart[fruit][:count] ||= 0
+      organized_cart[fruit][:count] += 1
+    end
+  end
+  return organized_cart
+end
+
+def consolidate_cart(cart)
   organized_cart = {}
   count = 0
   cart.each do |element|
-  #binding.pry
     element.each do |fruit, hash|
       organized_cart[fruit] ||= hash
       organized_cart[fruit][:count] ||= 0
@@ -16,22 +27,32 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons.each do |coupon_hash|
-    fruit_name = coupon_hash[:item]
-    new_coupon_hash = {
-      :price => coupon_hash[:cost],
-      :clearance => "true",
-      :count => coupon_hash[:num]
-    }
-
-     if cart.key?(fruit_name)
-      new_coupon_hash[:clearance] = cart[fruit_name][:clearance]
-      if cart[fruit_name][:count]>= new_coupon_hash[:count]
-        new_coupon_hash[:count] = (cart[fruit_name][:count]/new_coupon_hash[:count]).floor
-        cart[fruit_name][:count] = (coupon_hash[:num])%(cart[fruit_name][:count])
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
       end
-      cart[fruit_name + " W/COUPON"] = new_coupon_hash
+      cart[name][:count] -= coupon[:num]
     end
-    end
-  return cart
+  end
+  cart
+end
+
+
+
+def apply_clearance(cart)
+ cart.each do |fruit_name,fruit_hash|
+   if fruit_hash[:clearance] == true
+     fruit_hash[:price] = (fruit_hash[:price]*0.8).round(2)
+   end
+ end
+ return cart
+end
+
+def checkout(cart, coupons)
+  consolidated_cart = consolidate_cart(cart)
 end
