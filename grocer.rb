31196dcfe3164abff6aa9645f62b3d430e0dac09
Aliciea D Suechin <adr330@nyu.edu>
2019-06-19 22:04:15 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,60 @@
 def consolidate_cart(cart)
   # code here
+  newHash = {}
+  cart.each do |food, info|
+      food.each do |a,b|
+        if newHash[a]== nil
+          newHash[a]={price: b[:price], clearance: b[:clearance], count: 1}
+        else
+          newHash[a][:count]+=1
+        end
+      end
+  end
+  return newHash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  couponHash = {}
+	 amtHash = {}
+	 cCart =  cart
+	 if coupons == []
+		 return cCart
+	 end
+	 cCart.each do |food, info|
+		 coupons.each do |items|
+			 if items[:item] == food
+					 couponHash["#{food} W/COUPON"] = {:price => items[:cost], :clearance => info[:clearance], :count => getCouponCount(food,coupons)}
+           couponHash["#{food}"] = {:price => info[:price], :clearance => info[:clearance], :count => info[:count] - (items[:num]*getCouponCount(food,coupons))}
+			 end
+		 end
+	 end
+	 return cCart.merge!(couponHash)
 end
 
 def apply_clearance(cart)
   # code here
+  	cart.reject{|hash,info| info[:clearance]==false}.collect{|hash,info| info[:price]=((info[:price]*1.8)-info[:price]).round(1)}
+  return cart
 end
 
 def checkout(cart, coupons)
   # code here
-end
+  total = 0
+	cCart = apply_clearance(apply_coupons(consolidate_cart(cart),coupons))
+	
+	cCart.each do |food,info|
+	  if food == "BEER" || food == "BEER W/COUPON"
+	    total += info[:price]*info[:count]
+	  end
+	end
+	
+	if total > 100
+	  total =  ((total*1.9)-total).round(1)
+	  end
+	return total
+  end
+end 
+end 
+
+
